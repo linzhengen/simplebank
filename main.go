d2f8e440ba29fbb391f5a69af0e82d09f51c1aa5
linzhengen <linzhengen@yahoo.co.jp>
2022-09-06 23:03:35 +0900

    update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/golang-migrate/migrate/v4"
+	migrate "github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
